db98baf10c3cfdff02a2f25f6ad03fa5ee1c3554
Jonathan Roussel <jroussel@pps.co.za>
2026-03-26 12:44:16 +0200

    Added auth button for JWT and doc

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -2,6 +2,8 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +12,25 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI demoOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Spring Boot Demo API")
                         .version("1.0.0")
-                        .description("Demo API used to learn Maven, CI, Docker, and Swagger"));
+                        .description("Demo API with JWT-secured Swagger"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new io.swagger.v3.oas.models.Components()
+                                .addSecuritySchemes(
+                                        securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
     }
 }
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,10 +10,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.Map;
-
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @Tag(name = "Hello API", description = "Demo endpoints") //for swagger documentation
+@SecurityRequirement(name = "bearerAuth") // 👈 Swagger only
 public class HelloController {
 
 
