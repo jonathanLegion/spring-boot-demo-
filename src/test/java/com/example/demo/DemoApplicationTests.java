78a72813af6fef7bc75eb182025984b056b63ca5
Jonathan Roussel <jroussel@pps.co.za>
2026-03-27 13:00:28 +0200

    JWT stage 1 - url requires username and password at this point in time

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+
 
 @SpringBootTest
+@WithMockUser
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
