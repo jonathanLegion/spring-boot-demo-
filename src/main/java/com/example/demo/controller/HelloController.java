aaa45c8f4674a67757895d843690700527fd2613
Jonathan Roussel <jroussel@pps.co.za>
2026-03-27 17:09:00 +0200

    trying to create qa

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 
-
+ //comment just to create qa env
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.Map;
